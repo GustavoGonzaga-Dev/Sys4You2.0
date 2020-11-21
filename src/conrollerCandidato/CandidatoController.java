98a05d699f5577330c732e41ac055dd50b021655
dev <dev@dev-RV411-RV511-E3511-S3511-RV711-E3411>
2020-11-20 21:15:48 -0300

    Candidato

---
+++ src/conrollerCandidato/CandidatoController.java
@@ -0,0 +1,12 @@
+package conrollerCandidato;
+
+public class CandidatoController {
+	public String nome;
+	public String curso;
+	
+	public CandidatoController(String n, String c) {
+		this.nome = n;
+		this.curso = c;
+	}
+
+}
